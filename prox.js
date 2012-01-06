6c875ca229a91f50acb168e1e7e9192a46ec2f9e
Nick Orton <nick@orton.biz>
2012-01-06 13:04:46 +0000

    renaming curls to bots

--- prox.js
+++ prox.js
@@ -2,7 +2,7 @@ var http = require('http'),
 httpProxy = require('http-proxy');
 
 
-var agents = [
+var browsers = [
   "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7",
   "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7",
   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7",
@@ -11,31 +11,45 @@ var agents = [
   "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:8.0) Gecko/20100101 Firefox/8.0",
   "Mozilla/5.0 (Windows NT 5.1; rv:9.0.1) Gecko/20100101 Firefox/9.0.1" ];
 
-var curls = ["curl/7.7.2 (powerpc-apple-darwin6.0) libcurl 7.7.2 (OpenSSL 0.9.6b)",
-             "curl/7.21.0 (x86_64-apple-darwin10.2.0) libcurl/7.21.0 OpenSSL/1.0.0a zlib/1.2.5 libidn/1.19",
+var bots = ["curl/7.7.2 (powerpc-apple-darwin6.0) libcurl 7.7.2 (OpenSSL 0.9.6b)",
+            "curl/7.21.0 (x86_64-apple-darwin10.2.0) libcurl/7.21.0 OpenSSL/1.0.0a zlib/1.2.5 libidn/1.19",
             "curl/7.21.2 (i386-pc-win32) libcurl/7.21.2 OpenSSL/0.9.8o zlib/1.2.5",
             "curl/7.16.3 (powerpc-apple-darwin8.0) libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3",
-            "curl/7.16.1 (i386-pc-win32) libcurl/7.16.1 OpenSSL/0.9.8h zlib/1.2.3"];
+            "curl/7.16.1 (i386-pc-win32) libcurl/7.16.1 OpenSSL/0.9.8h zlib/1.2.3",
+            "Wget/1.9.1",
+            "Wget/1.10.1",
+            "wget",
+            "libwww-perl/5.808",
+            "libwww-perl/5.65",
+            "Mozilla/5.0 (compatible; YandexBot/3.0; +http://yandex.com/bots)",
+            "Wget/1.8.1" ];
 
-var curl_worthy = [/.*\.css/, /.*\.js/, /.*\.png/,
-                   /.*\.gif/, /.*\.ico/, /.*\.jpg/]
+var bot_worthy = [/.*\.css/, /.*\.js/, /.*\.png/,
+                  /.*\.gif/, /.*\.ico/, /.*\.jpg/,
+                  /.*\.flv/]
+
+var parent = {
+  host: 'localhost',
+  port: 3128
+}
 
 function select(arr){
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-httpProxy.createServer(function (req, res, proxy) {
- if(curl_worthy.some(function(regex){ return regex.test(req.url);})){
-   req.headers['user-agent'] = select(curls);
- } else {
+function set_user_agent(req, agents){
    req.headers['user-agent'] = select(agents);
- }
+}
 
- //console.log(req.headers['user-agent']);
- //console.log('##' +req.url)
+httpProxy.createServer(function (req, res, proxy) {
+  if(bot_worthy.some(function(regex){ return regex.test(req.url);})){
+    set_user_agent(req, bots);
+  } else {
+    set_user_agent(req, browsers);
+  }
 
- proxy.proxyRequest(req, res, {
-        host: 'localhost',
-        port: 3128
- });
+  //console.log(req.headers['user-agent']);
+  //console.log('##' +req.url)
+
+  proxy.proxyRequest(req, res, parent);
 }).listen(3129);
