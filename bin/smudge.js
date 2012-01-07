aeb6bd8a0615e9c37258563d6a18c486caf8ef8d
Nick Orton <nick@orton.biz>
2012-01-07 05:16:39 +0000

    adding comments.

--- bin/smudge.js
+++ bin/smudge.js
@@ -1,7 +1,7 @@
 var http = require('http'),
 httpProxy = require('http-proxy');
-conf = require('./conf')
-smudgers = require('./src/smudger')
+conf = require('../conf')
+smudgers = require('../lib/smudger')
 
 smudger = smudgers.create();
 
@@ -10,5 +10,5 @@ console.log('☠☠☠ waiting for headers to smudge ☠☠☠');
 
 httpProxy.createServer(function (req, res, proxy) {
   smudger.mutate(req);
-  proxy.proxyRequest(req, res, conf.parent);
+  proxy.proxyRequest(req, res, conf.target);
 }).listen(conf.listenPort);
--- conf.js
+++ conf.js
@@ -1,6 +1,10 @@
-exports.parent = {
+
+// The upstream proxy to which smudge will send requests.  In this example we
+// are targeting a squid proxy on local host.
+exports.target = {
   host: 'localhost',
   port: 3128
 }
 
+//the port to listen on
 exports.listenPort = 3129;
