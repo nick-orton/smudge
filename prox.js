1b77abe48a68a25ff5bfc50dba2dd8c74f276474
Nick Orton <nick@orton.biz>
2012-01-07 03:09:19 +0000

    moving user agents into seperate data file

--- prox.js
+++ prox.js
@@ -2,36 +2,8 @@ var http = require('http'),
 httpProxy = require('http-proxy');
 fs = require('fs');
 
-
-var browsers = [
-  "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7",
-  "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7",
-  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7",
-  "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:9.0.1) Gecko/20100101 Firefox/9.0.1",
-  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/534.52.7 (KHTML, like Gecko) Version/5.1.2 Safari/534.52.7",
-  "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:8.0) Gecko/20100101 Firefox/8.0",
-  "Mozilla/5.0 (Windows NT 5.1; rv:9.0.1) Gecko/20100101 Firefox/9.0.1" ];
-
-var bots = ["Baiduspider+(+http://www.baidu.com/search/spider_jp.html)",
-            "Baiduspider+(+http://www.baidu.com/search/spider.htm)",
-            "curl/7.21.2 (i386-pc-win32) libcurl/7.21.2 OpenSSL/0.9.8o zlib/1.2.5",
-            "curl/7.21.0 (x86_64-apple-darwin10.2.0) libcurl/7.21.0 OpenSSL/1.0.0a zlib/1.2.5 libidn/1.19",
-            "curl/7.16.3 (powerpc-apple-darwin8.0) libcurl/7.16.3 OpenSSL/0.9.7l zlib/1.2.3",
-            "curl/7.16.1 (i386-pc-win32) libcurl/7.16.1 OpenSSL/0.9.8h zlib/1.2.3",
-            "curl/7.7.2 (powerpc-apple-darwin6.0) libcurl 7.7.2 (OpenSSL 0.9.6b)",
-            "libwww-perl/5.808",
-            "libwww-perl/5.65",
-            "Mozilla/5.0 (compatible; YandexBot/3.0; +http://yandex.com/bots)",
-            "PycURL/7.19.5",
-            "PycURL/7.18.2",
-            "PycURL/7.13.2",
-            "Wget/1.10.1",
-            "Wget/1.9.1",
-            "Wget/1.8.1"
-            ];
-
-var fileContents = fs.readFileSync("./langs.json",'utf8');
-var langs = JSON.parse(fileContents);
+var langs = JSON.parse(fs.readFileSync("./langs.json",'utf8'));
+var users = JSON.parse(fs.readFileSync("./users.json",'utf8'));
 
 var bot_worthy = [/.*\.css/, /.*\.js/, /.*\.png/,
                   /.*\.gif/, /.*\.ico/, /.*\.jpg/,
@@ -69,9 +41,9 @@ function set_user_agent(req, agents){
 
 httpProxy.createServer(function (req, res, proxy) {
   if(bot_worthy.some(function(regex){ return regex.test(req.url);})){
-    set_user_agent(req, bots);
+    set_user_agent(req, users["bots"]);
   } else {
-    set_user_agent(req, browsers);
+    set_user_agent(req, users["browsers"]);
   }
   req.headers['accept-language'] = accept_language();
 
